a28d4fbedbd41527b6f2709743b68b6b526113fe
NiniLobzhanidze <lobzhanidze264@gmail.com>
2024-11-24 13:51:20 +0400

    davaleba3

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str= "I Love Software Testing";
+
+        for (int i=0; i <=str.length(); i++);{
+            if (str.length() % 2 == 0)
+
+                System.out.println("არსებული სტრიქონის სიგრძე არის ლუწი");
+            else
+                System.out.println("არსებული სტრიქონი არის კენტი");
+            }
+
+        }
+    }
